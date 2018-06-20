1f5386dde5d4224704aaaccf0afb7e2d4084dd1a
Breno <breno@dcc.ufmg.br>
2018-06-20 11:01:01 -0300

    fatoracao adicionada

--- rsa.py
+++ rsa.py
@@ -19,6 +19,37 @@ def mdc_estendido(a,b):
     return (y,x-(a//b)*y)
 
 
-print(mdc_estendido(13,2))
+def fatora(n):#usado para fatorar um n-1 em potencia de dois em miller
+    atual = 2
+    contador = 0
+    t=0
+    while atual*atual <= n:
+        while(n%atual == 0):
+            if atual == 2:
+                t+=1
+            n/=atual
+        atual+=1
+
+    return (t,int(n))#retorna o o numero fatorado em base 2 x parte prima
+
+def miller(a,n):
+    x=exp_rapida(a,q,n)
+    if x==1 or x==-1:
+        return "Nao sei"
+    while(t>0):
+        x = x*x%n
+        if x==n-1:
+            return "Nao sei"
+        t-=1
+    return "composto"
+
+
+print(fatora(320))
+
+
+
+
+
+# print(mdc_estendido(13,2))
 # print(pow(5,1024))
 # print(exp_rapida(5,1024,2))
